2432d5b90aecf735a40dc4121f606b3205c5c85b
Ajaya Mishra <azaya.mishra@gmail.com>
2019-12-18 17:44:04 +0545

    Adding SASS and creating homepage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import './client/styles/pages/components/homepage.styles.scss'
+
+import HomePage from './client/components/pages/homepage'
 
 function App() {
   return (
-    <div>
-      Hello Gaijin Sewa
-    </div>
+    <HomePage />
   );
 }
 
