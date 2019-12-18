6fd17cfe2fb44be68de3377cb267cc1b33d027d9
Ajaya Mishra <azaya.mishra@gmail.com>
2019-12-18 18:26:00 +0545

    moving codes to components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import './client/styles/pages/components/homepage.styles.scss'
+import './client/styles/common/home-directory/home-directory.styles.scss'
+import './client/styles/common/home-menu-item/home-menu-item.styles.scss'
 
 import HomePage from './client/components/pages/homepage'
 
---
+++ src/client/components/common/home-directory/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import HomeMenuItem from '../home-menu-item'
+
+const HomeDirectory = () => (
+  <div className="directory-menu">
+    <HomeMenuItem title='HATS' />
+    <HomeMenuItem title='JACKETS' />
+    <HomeMenuItem title='SNEAKERS' />
+    <HomeMenuItem title='MENS' />
+    <HomeMenuItem title='WOMENS' />
+  </div>
+)
+
+export default HomeDirectory
