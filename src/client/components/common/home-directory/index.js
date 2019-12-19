c4ead009e37fe9ba247cb6bfa0f0e302ef828a2f
Ajaya Mishra <azaya.mishra@gmail.com>
2019-12-19 17:31:23 +0545

    adding image and styles to the homepage images

--- src/client/components/common/home-directory/index.js
+++ src/client/components/common/home-directory/index.js
@@ -2,13 +2,52 @@ import React from 'react'
 
 import HomeMenuItem from '../home-menu-item'
 
+const sections = [
+  {
+    title: 'hats',
+    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    id: 1,
+    linkUrl: 'shop/hats'
+  },
+  {
+    title: 'jackets',
+    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    id: 2,
+    linkUrl: 'shop/jackets'
+  },
+  {
+    title: 'sneakers',
+    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    id: 3,
+    linkUrl: 'shop/sneakers'
+  },
+  {
+    title: 'womens',
+    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    size: 'large',
+    id: 4,
+    linkUrl: 'shop/womens'
+  },
+  {
+    title: 'mens',
+    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    size: 'large',
+    id: 5,
+    linkUrl: 'shop/mens'
+  }
+]
+
 const HomeDirectory = () => (
   <div className="directory-menu">
-    <HomeMenuItem title='HATS' />
-    <HomeMenuItem title='JACKETS' />
-    <HomeMenuItem title='SNEAKERS' />
-    <HomeMenuItem title='MENS' />
-    <HomeMenuItem title='WOMENS' />
+    {sections.map(({ title, imageUrl, id, linkUrl, size}) => (
+      <HomeMenuItem
+        key={id}
+        title={title}
+        imageUrl={imageUrl}
+        linkUrl={linkUrl}
+        size={size}
+      />
+    ))}
   </div>
 )
 
